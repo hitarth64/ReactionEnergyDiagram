5da60afe25e1be14a6f61647627ad22ed906d2f0
Hitarth Choubisa <hitarthc64@gmail.com>
2022-08-30 13:50:22 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ReactionEnergyDiagaram',
+      version='0.0',
+      description='Python package to plot reaction energy diagrams',
+      author='Hitarth Choubisa',
+      author_email='hitarthc64@gmail.com',
+      url='https://github.com/hitarth64/ReactionEnergyDiagram',
+      packages=['ReactionEnergyDiagaram'],
+     )
